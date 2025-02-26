e640446c227104d2dc4ee3d2e17e59a08dec630a
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2025-02-26 17:11:18 -0600

    Update style.css and script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.container');
+const toggleMode = document.querySelector('.toggle-mode');
+const toggleModeIcon = document.querySelector('.toggle-mode__icon');
+const logoIcon = document.querySelector('.logo__icon');
+
+const info = document.querySelector('.info');
+
+const textArea = document.getElementById('textarea-custom');
+const excludeSpaces = document.getElementById('exclude-spaces');
+const characterLimit = document.getElementById('character-limit');
+
+const includeSpacesText = document.querySelector('.counter__sub-title-spaces');
+const readingTimeCounter = document.querySelector('.reading-time__counter');
+
+
+
+
+
+
+
+
