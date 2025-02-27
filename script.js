c572de041ebc32c7faf852be603ad31f9ffaa789
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2025-02-27 16:23:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,26 @@ const characterLimit = document.getElementById('character-limit');
 const includeSpacesText = document.querySelector('.counter__sub-title-spaces');
 const readingTimeCounter = document.querySelector('.reading-time__counter');
 
+const counterCharacters = document.querySelector('.counter__characters');
+const counterWords = document.querySelector('.counter__words');
+const counterSentences = document.querySelector('.counter__sentences');
+const characterLimitInput = document.querySelector('.counter-character__input');
+const maxChars = document.querySelector('.max-characters');
+const noChars = document.querySelector('.no-characters');
+
+const densityContainer = document.querySelector('.density__container');
+
+const charactersList = {}
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+let totalCharacters = 0;
+let totalWords = 0;
+let totalSentences = 0;
+let charactersLimit = 0;
+
+for (let char of alphabet) {
+  charactersList[char] = 0;
+}
 
 
 
